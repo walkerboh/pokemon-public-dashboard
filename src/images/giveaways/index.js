5c586247c170a9fed593e049afbf78138fa5e4ee
Evan Frisch <walkerboh1213@gmail.com>
2019-11-01 09:52:06 -0400

    updated images

--- src/images/giveaways/index.js
+++ src/images/giveaways/index.js
@@ -1,29 +1,21 @@
 import Falkner from "./Falkner.png";
 import Bugsy from "./Bugsy.png";
 import Whitney from "./Whitney.png";
-import Giovanni from "./Giovanni.png";
-import Koga from "./Koga.png";
-import Lavender from "./Lavender.png";
-import League from "./League.png";
-import Misty from "./Misty.png";
-import Rocket1 from "./Rocket1.png";
-import Rocket2 from "./Rocket2.png";
-import Sabrina from "./Sabrina.png";
-import Surge from "./Surge.png";
+import Morty from "./Morty.png";
+import Chuck from "./Chuck.png";
+import Jasmine from "./Jasmine.png";
+import Pryce from "./Pryce.png";
+import Clair from "./Clair.png";
 
 const giveawayImages = {
   Falkner,
   Bugsy,
   Whitney,
-  Giovanni,
-  Koga,
-  Lavender,
-  League,
-  Misty,
-  Rocket1,
-  Rocket2,
-  Sabrina,
-  Surge
+  Morty,
+  Chuck,
+  Jasmine,
+  Pryce,
+  Clair
 };
 
 export default giveawayImages;
